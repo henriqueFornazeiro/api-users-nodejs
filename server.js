6c90b0a0807a994dbb511b1ec9177e8bb88c9b02
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2024-11-15 17:08:46 -0300

    feat: rota put para edicao de usuario

--- server.js
+++ server.js
@@ -7,31 +7,60 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/usuarios', async (req, res)=>{
+app.post('/usuarios', async (req, res) => {
 
     try {
         const user = {
             email: req.body.email,
             name: req.body.name,
             age: req.body.age
-         }
-    
+        }
+
         await prisma.user.create({
             data: user
         })
-    
+
         res.status(201).json(user)
-    } catch (error) {        
+    } catch (error) {
         res.status(400).send("Erro ao tentar criar usuário.")
     }
-    
+
 })
 
-app.get('/usuarios', async (req,res)=>{
-    const allUsers = await prisma.user.findMany()
+app.get('/usuarios', async (req, res) => {
+    try {
 
-    res.status(200).json(allUsers)
+        const allUsers = await prisma.user.findMany()
+
+        res.status(200).json(allUsers)
+    } catch (error) {
+
+        res.status(400).send("Erro ao tentar listar usuários.")
+
+    }
 })
 
+app.put('/usuarios/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+        const user = {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+
+        await prisma.user.update({
+            where: { id: id},
+            data: user
+        })
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(400).send("Erro ao tentar editar usuário.")
+    }
+
+})
 
 app.listen(3000)
