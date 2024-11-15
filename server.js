83896511de5e8ca9b4005e7ba784534e73fb9e3c
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2024-11-15 16:50:18 -0300

    feat: conexao mongo e rota post

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', (req, res)=>{
-    users.push(req.body)
-    res.status(201).json(req.body)
+app.post('/usuarios', async (req, res)=>{
+    await prisma.user.create({
+        data:{
+           email: req.body.email,
+           name: req.body.name,
+           age: req.body.age
+        }
+    })
 })
 
-app.get('/usuarios', (req,res)=>{
-
-    res.status(200).json(users)
-})
 
 app.listen(3000)
